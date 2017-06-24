5cd2bb43b5451d8062f937550525cbeeaa806ed2
Kyle E. Mitchell <kyle@kemitchell.com>
2017-06-23 21:27:55 -0700

    Test codename uniqueness

--- test.js
+++ test.js
@@ -9,12 +9,22 @@ if (validator.errors) {
   console.error(JSON.stringify(validator.errors, null, 2))
 }
 assert.deepEqual(validator.errors, null)
+var codenames = []
 challenges.forEach(function (challenge) {
   if (challenge.readOnly) {
     challenge.readOnly.forEach(function (line) {
       assert(
         typeof challenge.code[line] === 'string'
       )
+    })
+  }
+  if (challenge.codenames) {
+    challenge.codenames.forEach(function (codename) {
+      assert.equal(
+        codenames.indexOf(codename), -1,
+        'codename "' + codename + '" used more than once'
+      )
+      codenames.push(codename)
     })
   }
 })
